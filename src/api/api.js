e532c10e4f8848229f8c010b93e437d622a0a5ea
liuxy0551 <liuxy0551@qq.com>
2018-09-16 22:40:55 +0800

    Init

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,14 @@ import {
   wxRequest
 } from '@/utils/wxRequest';
 
-const debug = false;
+const host = 'https://h878.cn';
+
+const register = params => wxRequest(params, host+'/group/meal/user/register');
+// 用户协议
+const getText = () => wxRequest('', host+'/group/meal/other/disclaimer');
+
 
 export default {
-
+  register,
+  getText,
 }
