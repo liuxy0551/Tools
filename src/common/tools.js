8da2a53505df351fa248b14e7eb328969c3bb003
Liuxy <liuxy0551@qq.com>
2018-11-06 18:24:35 +0800

    镜子、图标

--- src/common/tools.js
+++ src/common/tools.js
@@ -11,49 +11,49 @@ let tools = {
       id: "1",
       name: "系统资源",
       url: "systemResources",
-      icon: "../../images/tools/fliqlo.png"
+      icon: "../../images/tools/systemResources.png"
     },
     {
       id: "2",
       name: "尺子",
       url: "rules",
-      icon: "../../images/tools/fliqlo.png"
+      icon: "../../images/tools/rules.png"
     },
     {
       id: "3",
       name: "人民币大写",
       url: "capitalRMB",
-      icon: "../../images/tools/fliqlo.png"
+      icon: "../../images/tools/capitalRMB.png"
     },
     {
       id: "4",
       name: "生命计算",
       url: "lifeCalculation",
-      icon: "../../images/tools/fliqlo.png"
+      icon: "../../images/tools/lifeCalculation.png"
     },
     {
       id: "5",
       name: "亲戚称呼计算",
       url: "relative",
-      icon: "../../images/tools/fliqlo.png"
+      icon: "../../images/tools/relative.png"
     },
     {
       id: "6",
       name: "生成二维码",
       url: "generateQrCode",
-      icon: "../../images/tools/fliqlo.png"
+      icon: "../../images/tools/generateQrCode.png"
     },
     {
       id: "7",
       name: "识别二维码",
       url: "identifyQrCode",
-      icon: "../../images/tools/fliqlo.png"
+      icon: "../../images/tools/identifyQrCode.png"
     },
     {
       id: "8",
       name: "真心话大冒险",
       url: "truthIsDangerous",
-      icon: "../../images/tools/fliqlo.png"
+      icon: "../../images/tools/truthIsDangerous.png"
     }
   ],
   two: [
@@ -61,19 +61,25 @@ let tools = {
       id: "9",
       name: "BMI指数",
       url: "BMI",
-      icon: "../../images/tools/fliqlo.png"
+      icon: "../../images/tools/BMI.png"
     },
     {
       id: "10",
       name: "税后计算器",
       url: "afterTaxCalculator",
-      icon: "../../images/tools/fliqlo.png"
+      icon: "../../images/tools/afterTaxCalculator.png"
     },
     {
       id: "11",
       name: "镜子",
       url: "mirror",
-      icon: "../../images/tools/fliqlo.png"
+      icon: "../../images/tools/mirror.png"
+    },
+    {
+      id: "12",
+      name: "计算器",
+      url: "calculator",
+      icon: "../../images/tools/calculator.png"
     }
   ]
 };
