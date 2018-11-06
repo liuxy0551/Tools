829a9eea483dad70aa19b18d29a092320f9ad916
Liuxy <liuxy0551@qq.com>
2018-11-06 13:32:00 +0800

    新建工具页面

---
+++ src/common/tools.js
@@ -0,0 +1,85 @@
+// 本地模拟json数据
+let tools = {
+  one: [
+    {
+      id: "0",
+      name: "翻页时钟",
+      url: "fliqlo",
+      icon: "../../images/tools/fliqlo.png"
+    },
+    {
+      id: "1",
+      name: "系统资源",
+      url: "systemResources",
+      icon: "../../images/tools/fliqlo.png"
+    },
+    {
+      id: "2",
+      name: "尺子",
+      url: "rules",
+      icon: "../../images/tools/fliqlo.png"
+    },
+    {
+      id: "3",
+      name: "人民币大写",
+      url: "capitalRMB",
+      icon: "../../images/tools/fliqlo.png"
+    },
+    {
+      id: "4",
+      name: "生命计算",
+      url: "lifeCalculation",
+      icon: "../../images/tools/fliqlo.png"
+    },
+    {
+      id: "5",
+      name: "亲戚称呼计算",
+      url: "relative",
+      icon: "../../images/tools/fliqlo.png"
+    },
+    {
+      id: "6",
+      name: "生成二维码",
+      url: "generateQrCode",
+      icon: "../../images/tools/fliqlo.png"
+    },
+    {
+      id: "7",
+      name: "识别二维码",
+      url: "identifyQrCode",
+      icon: "../../images/tools/fliqlo.png"
+    },
+    {
+      id: "8",
+      name: "真心话大冒险",
+      url: "truthIsDangerous",
+      icon: "../../images/tools/fliqlo.png"
+    }
+  ],
+  two: [
+    {
+      id: "9",
+      name: "BMI指数",
+      url: "BMI",
+      icon: "../../images/tools/fliqlo.png"
+    },
+    {
+      id: "10",
+      name: "税后计算器",
+      url: "afterTaxCalculator",
+      icon: "../../images/tools/fliqlo.png"
+    },
+    {
+      id: "11",
+      name: "镜子",
+      url: "mirror",
+      icon: "../../images/tools/fliqlo.png"
+    }
+  ]
+};
+
+// 定义数据出口
+module.exports = {
+  tools: tools
+};
+
